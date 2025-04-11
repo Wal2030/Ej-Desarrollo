558d3465b25601478668f66c8d214b7f30513f1f
walter2030 <walterfelipe1403mail.com>
2025-04-10 21:04:54 -0500

    prueba 101

--- netlify/functions/Prueba.js
+++ netlify/functions/Prueba.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const empresasroutes = require("../../backend/routes/empresasroutes");
 
 const app = express();
 
 app.use(cors());
-
-app.use(express.json());
+app.use(bodyParser.json()); // 👈 añade esto
+// app.use(express.json()); // Puedes comentar este si usas body-parser
 
 app.use("/.netlify/functions/Prueba", empresasroutes);
 
