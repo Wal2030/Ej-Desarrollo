e06b9f3b3a6b2188c28ed1e40c50bb7e92a3d802
walter2030 <walterfelipe1403mail.com>
2025-05-31 16:07:43 -0500

    ya 2

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // Configuración de Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-app.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-app.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+    apiKey: "AIzaSyBjAcyxL-7ZuLV5saWtTzJt3g_hChn_Ync",
+    authDomain: "mipymes-evaluacion.firebaseapp.com",
+    projectId: "mipymes-evaluacion",
+    storageBucket: "mipymes-evaluacion.appspot.com",
+    messagingSenderId: "654910430175",
+    appId: "1:654910430175:web:9c287dc8536e17e0e6f3b4"
 };
 
 // Inicializar Firebase
