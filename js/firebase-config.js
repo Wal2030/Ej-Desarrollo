47b9489a8cf3696036b19f34fd3869d47cda09ca
walter2030 <walterfelipe1403mail.com>
2025-05-31 16:16:02 -0500

    ya 2

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,16 +5,26 @@ const firebaseConfig = {
     projectId: "mipymes-evaluacion",
     storageBucket: "mipymes-evaluacion.appspot.com",
     messagingSenderId: "654910430175",
-    appId: "1:654910430175:web:9c287dc8536e17e0e6f3b4"
+    appId: "1:654910430175:web:9c287dc8536e17e0e6f3b4",
+    measurementId: "G-MEASUREMENT_ID"
 };
 
 // Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
 
 // Referencias a servicios de Firebase
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
+
+// Configuración de Firestore
+db.settings({
+    timestampsInSnapshots: true
+});
 
 // Exportar para uso en otros archivos
 window.db = db;
