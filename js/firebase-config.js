6f637693bdf2c149f867ca6c4300e16585ed867c
walter2030 <walterfelipe1403mail.com>
2025-06-01 19:31:53 -0500

    prueba 5

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,21 @@
 // Configuración de Firebase
+/* 
+ * IMPORTANTE: Debes reemplazar estos valores con las credenciales reales de tu proyecto Firebase
+ * Para obtener estas credenciales:
+ * 1. Ve a https://console.firebase.google.com
+ * 2. Selecciona tu proyecto "retoevaluacion"
+ * 3. Ve a la configuración del proyecto (ícono de engranaje)
+ * 4. En "Configuración general", encontrarás estos valores
+ */
 const firebaseConfig = {
-    apiKey: "REEMPLAZA_CON_TU_API_KEY",
-    authDomain: "retoevaluacion.firebaseapp.com",
-    projectId: "retoevaluacion",
-    storageBucket: "retoevaluacion.appspot.com",
-    messagingSenderId: "REEMPLAZA_CON_TU_MESSAGING_SENDER_ID",
-    appId: "REEMPLAZA_CON_TU_APP_ID"
-};
+    apiKey: "AIzaSyCeJARHI7JwRb_nqypXdb56wEOG5oKtp3Y",
+    authDomain: "reto-mipymes.firebaseapp.com",
+    projectId: "reto-mipymes",
+    storageBucket: "reto-mipymes.firebasestorage.app",
+    messagingSenderId: "755553843367",
+    appId: "1:755553843367:web:2ac35c5e52506f669bc382",
+    measurementId: "G-15N6EZDK5N"
+  };
 
 // Función para inicializar Firebase
 function initializeFirebase() {
